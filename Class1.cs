c49622b69e3e6db1f1d5d63c588bae1aa4147d24
Thitikorn V <thitikorn@intinsights.com>
2020-11-19 11:37:16 +0700

    update source sub project

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace submodulestest
+{
+    public class Class1
+    {
+        private string Name { get; set; }
+        private string LastName { get; set; }
+        private string Occupation { get; set; }
+
+        public Class1(string name, string lastname)
+        {
+            Name = name;
+            LastName = lastname;
+        }
+        
+        public string GetFullname()
+        {
+            return $"{Name} {LastName}";
+        }
+
+        public string GetOccupation(string occupation)
+        {
+            return occupation;
+        }
+    }
+}
